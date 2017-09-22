23b7f9105efd024e2daf205c87c4278c449da83a
Kristian <kristian_persson@yahoo.com>
2017-09-22 13:14:17 +0200

    Version 0.33 2017-09-22
    Flea market add new rows when leaving post id field. Calculates the sum to pay automaticly.
    New remove row button in flea market.
    Displays the name for each post at the input row.
    
    modified:   CHANGELOG
    modified:   auktion.db3
    modified:   static/js/flea_market.js
    modified:   templates/flea_market.html
    modified:   uaf.py

--- static/js/flea_market.js
+++ static/js/flea_market.js
@@ -15,15 +15,43 @@ $(document).ready(function(){
         });
     };
 
-    // Fetch json data for the selected post when user presses tab in the post_id input.
-    var func = function(post_id, price_id){
+    // Handlers for the items in the input rows
+    var func = function(id){    
+        console.log(id);
+//        var row_id = "row" + id;
+        var post_id = "post" + id;
+        var price_id = "price" + id;
+        var info_id = "info" + id; 
+        var remove_id = "remove" + id;
+        console.log("Remove: " + remove_id, id);
+        
+        // Leave price input
+         $('#' + price_id).blur(function() {
+            calculate_sum();
+            
+         });
+        
+         $('#' + price_id).change(function() {
+            calculate_sum();
+         });
+        
+        // Removes a row in the form
+        $('#' + remove_id).click(function() {
+            var row_id = "row" + id;
+            console.log("Remove: " + row_id);
+//            $('#' + row_id).html("").fadeIn();
+            $( "#" + row_id).remove();
+            calculate_sum();
+        });
 
+        // Fetches info about the post and displays
         $('#' + post_id).blur(function() {
-
-            var id = $('#' + post_id).val();
+            add_row();
+            var cur_post_id = $('#' + post_id).val();
+            var price_id = "price" + id;
 
             $.ajax({
-                url: 'json/' + id,
+                url: 'json/' + cur_post_id,
                 dataType: 'json',
                 success: function(data){
                     if(data.hasOwnProperty('error')){
@@ -38,6 +66,8 @@ $(document).ready(function(){
                         $('#error').html("POSTEN FINNS INTE I DATABASEN").fadeIn();
                     } else {
                         console.log("Found");
+                        console.log(info_id)
+                        $("#" + info_id).html([data.scientific_name, data.plain_name].join(" ")).fadeIn();
                         $("#" + price_id).val(data.fixed_price);
                         $('#scientific_name').html(data.scientific_name).fadeIn();
                         $('#plain_name').html(data.plain_name).fadeIn();
@@ -51,12 +81,14 @@ $(document).ready(function(){
                         }
                      if (data.sold_on !== null) {
                          $('#sold').html("Redan sålt").fadeIn();
-                         document.getElementById("price").value = data.sold_price;
+                         console.log("price_id: " + price_id + " id: "  + id + " sold for: " + data.sold_price);
+                         $("#" + price_id).val(data.sold_price);
                      } else {
                          $('#sold').html("").fadeIn();
-                         document.getElementById("price").value = "";
+//                         document.getElementById("#" + price_id).value = "";
                      }
-                        $('#error').html("").fadeIn();
+                     $('#error').html("").fadeIn();
+                     $("#" + price_id).trigger("change");
                     };
                     console.log('.ajax() request returned successfully.');
                 },
@@ -76,34 +108,64 @@ $(document).ready(function(){
     };
 
 
-    // Add a new row of input boxes
+    // Handler for add button
     var rowNum = 1;
     $("#addbutton").click(function(){
         console.log("addbutton clicked");
+        add_row();
+    });
+    
+    // Add a new row of input boxes
+    function add_row() {
         rowNum ++;
+        var row_id = "row" + rowNum;
         var post_id = "post" + rowNum;
         var price_id = "price" + rowNum;
-
-        $("#form_elements").append('<div class="row"><div class="col-sm-2"><label for="' + post_id + '">Post nr:</label> <input class="loppis" id="' + post_id + '" name="post_id" type="text" value=""></div> <div class="col-sm-2"> <label for="' + price_id + '">Pris:</label>  <input class="loppis price" id="' + price_id + '" name="price" type="text" value=""></div></div><br>');
+        var info_id = "info" + rowNum; 
+        var remove_id = "remove" + rowNum;
+        var html_str = '<div id="' + row_id + '">' + 
+                            '<div class="row" >' + 
+                                '<div class="col-sm-2">' + 
+                                    '<input class="loppis form-control" id="' + post_id + '" name="post_id" type="text" value="">' +
+                                '</div>' +
+                                '<div class="col-sm-2">' +
+                                    '<input class="loppis price form-control" id="' + price_id + '" name="price" type="text" value="">' +
+                                '</div>' +
+                                '<div class="col-sm-6">' +
+                                    '<div class="form-control" id="' + info_id + '"> </div>' +
+                                '</div>' +  
+                                '<div class="col-sm-1">' +
+                                    '<button type="button" id="' + remove_id + '" class="btn btn-danger" tabindex="-1">Ta bort <span class="glyphicon glyphicon-remove"></span></button>' +
+                                '</div>' +
+                            '</div>' +
+                            '<br>' +
+                       '</div>';
         
-        func(post_id, price_id);
-    });
+        $("#form_elements").append(html_str);
+        // Add handler to items in the new row
+        func(rowNum);
+    };
 
 
-    // Caclulate the sum the customer should pay
+    // Handler for sumbutton
     $("#sumbutton").click(function(){
+        calculate_sum();  
+    });
+    
+    // Caclulate the sum the customer should pay    
+    function calculate_sum() {
         var tot_sum = 0;
         $('.price').each(function(i, obj) {
             console.log(this.value);
             tot_sum += Number(this.value);
             console.log(tot_sum);
             $('#sum').html("Att betala: " + tot_sum).fadeIn();
-        });
-    })
+        });   
+    }
 
 
     // Set handler for first post
-    func("post1", "price1");
+    func(1);
 
     console.log('Everything is ready.');
 });
--- uaf.py
+++ uaf.py
@@ -32,7 +32,7 @@ __author__ = 'Kristian'
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "HK(9045hjfd204hHFD345d"
 DATABASE = "auktion.db3"
-VERSION = "0.32"
+VERSION = "0.33"
 
 # For flask-login
 lm = LoginManager()
