8eef47841d9cb13435a747605e7b13ca3fe9f5de
Kristian <kristian_persson@yahoo.com>
2017-11-01 21:16:36 +0100

    Version 0.43 2017-11-01
    Updated version nr, which I forgot to update.
    
    modified:   uaf.py

--- uaf.py
+++ uaf.py
@@ -32,7 +32,7 @@ __author__ = 'Kristian'
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "HKd(9045hjffdd204hHFD345d"
 DATABASE = "auktion.db3"
-VERSION = "0.42"
+VERSION = "0.43"
 
 # For flask-login
 lm = LoginManager()
