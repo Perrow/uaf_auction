bd0ff4f3c9f5ad4f78e9a481b98451fa2894a25a
Perrow <per.trelje@gmail.com>
2026-09-17 21:07:24 +0200

    Registrera betalningsrapportens PDF-route

--- run_dev.py
+++ run_dev.py
@@ -5,6 +5,7 @@
 from uaf import app
 import label_preview
 import payment_records
+import payment_report_pdf
 import post_limit
 import swish_qr
 
@@ -12,6 +13,7 @@ import swish_qr
 label_preview.register_routes(app)
 post_limit.register_routes(app)
 payment_records.register_routes(app)
+payment_report_pdf.register_routes(app)
 swish_qr.register_routes(app)
 
 
